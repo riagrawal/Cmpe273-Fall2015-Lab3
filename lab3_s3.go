99cfb137b1595303438e2c451e6cc788248c8025
unknown <richa.agrawal@sjsu.edu>
2015-11-20 11:33:18 -0800

    server 3

--- lab3_s3.go
+++ lab3_s3.go
@@ -1,5 +1,91 @@
 package main 
 
+import (
+	"github.com/julienschmidt/httprouter"
+    "net/http"
+    "log"
+    "strconv"
+    "encoding/json"
+)
+
+var key_value map[int] string
+
+type Response struct{
+	Key  int 		`json:"key"`
+	Value string    `json:"value"`
+
+}
+
 func main(){
-	//server 3 code
+	//server 1 code
+	/*server := rpc.NewServer()
+	server.Register(cal)
+	server.HandleHTTP(rpc.DefaultRPCPath, rpc.DefaultDebugPath)
+	listener, e := net.Listen("tcp", ":3000")
+	if e != nil {
+		log.Fatal("listen error:", e)
+	}
+	for {
+		if conn, err := listener.Accept(); err != nil {
+			log.Fatal("accept error: " + err.Error())
+		} else {
+			log.Printf("new connection established\n")
+			go server.ServeCodec(jsonrpc.NewServerCodec(conn))
+		}
+	}*/
+	key_value = make(map[int] string)
+	mux := httprouter.New()
+    mux.PUT("/keys/:id/:value",put)
+    mux.GET("/keys/:id",get)
+    mux.GET("/keys",getall)
+    server := http.Server{
+            Addr:        "0.0.0.0:3002",
+            Handler: mux,
+    }
+    server.ListenAndServe()
+}
+
+func put(rw http.ResponseWriter, req *http.Request, p httprouter.Params){
+	log.Println("Inside Put!!")
+	key := p.ByName("id")
+	value := p.ByName("value")
+	key_int, _ := strconv.Atoi(key)
+	key_value[key_int] = value
+}
+
+func get(rw http.ResponseWriter, req *http.Request, p httprouter.Params){
+	log.Println("Inside GET!!")
+	key := p.ByName("id")
+	key_int, _ := strconv.Atoi(key)
+	var response Response
+    response.Key = key_int
+    response.Value = key_value[key_int]
+  	payload, err := json.Marshal(response)  
+  	if err != nil {
+    	 http.Error(rw,"Bad Request" , http.StatusInternalServerError)
+     	return
+  	}
+  	rw.Header().Set("Content-Type", "application/json")
+  	rw.Write(payload)
+}
+
+
+func getall(rw http.ResponseWriter, req *http.Request, p httprouter.Params){
+	log.Println("Inside GETALL!!")
+	var response []Response
+	var key_pair Response
+	for key, value := range key_value {
+    	//fmt.Println("Key:", key, "Value:", value)
+    	key_pair.Key = key
+    	key_pair.Value = value
+		response = append(response, key_pair)
+	}
+    
+  	payload, err := json.Marshal(response)  
+  	if err != nil {
+    	 http.Error(rw,"Bad Request" , http.StatusInternalServerError)
+     	return
+  	}
+  	rw.Header().Set("Content-Type", "application/json")
+  	rw.Write(payload)
 }
